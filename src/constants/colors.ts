2140dcef7e96c8a8d2e1528585be42f09521f355
RAJESH2132 <rajeshnagurla@gmail.com>
2025-08-15 15:39:42 +0530

    done with project setup and native wind setup

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export const COLORS = {
+  primary: "#0F171E",     // Deep blue background
+  accent: "#00A8E1",      // Prime Video blue
+  background: "#121A22",  // Dark grayish
+  text: "#FFFFFF",        // White text
+  muted: "#A1A1A1",       // Light gray text
+};
